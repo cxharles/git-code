c25f2fd9a62e17dd4e865699511412948c81e40f
Charles Jatto <jatto_charles@yahoo.com>
2023-03-10 21:40:38 -0500

    Updated DBConnect.java file

---
+++ DBConnect.java
@@ -0,0 +1,12 @@
+// Ask for the user's name
+let name = prompt("What is your name?");
+
+// Display a greeting using the user's name
+alert("Hello, " + name + "!");
+
+// Ask for the user's address
+let address = prompt("What is your address?");
+
+// Display the user's address in the console
+console.log("The user's address is: " + address);
+
